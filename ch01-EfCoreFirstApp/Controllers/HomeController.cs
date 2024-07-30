ef5971c1af82b65df3033c29860c318e418dd9c6
alexxx <puyaalex@gmail.com>
2024-07-30 22:28:34 +0300

    first commit

---
+++ ch01-EfCoreFirstApp/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using EfCoreFirstApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EfCoreFirstApp.Controllers;
+
+public class HomeController : Controller
+{
+    private DataContext _context;
+
+    public HomeController(DataContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index() => View();
+
+    public IActionResult Respond() => View();
+
+    [HttpPost]
+    public IActionResult Respond(GuestResponse response)
+    {
+        _context.Responses.Add(response);
+
+        _context.SaveChanges();
+
+        return RedirectToAction(nameof(Thanks), new { Name = response.Name, WillAttend = response.WillAttend });
+    }
+
+    public IActionResult Thanks(GuestResponse response) => View(response);
+
+    public IActionResult ListResponses() =>
+        View(_context.Responses.OrderByDescending(r => r.WillAttend));
+}
---
+++ ch01-EfCoreFirstApp/Migrations/20240730184504_Initial.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ch01EfCoreFirstApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Responses",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WillAttend = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Responses", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Responses");
+        }
+    }
+}
